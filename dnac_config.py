bb71bf0e94dc463e4a3d90ef4f7d8b9155a60e7f
rsayle <rsayle@cisco.com>
2019-01-21 22:16:40 -0800

    Add files via upload

---
+++ dnac_config.py
@@ -0,0 +1,44 @@
+#
+# dnac_config.py - configuration parameters for a Dnac object
+#
+
+'''
+DNAC_NAME: FQDN of a DNAC cluster
+           A Dnac instance prefers this value over an IP address (DNAC_IP)
+           To use an IP address instead, set this constant = ""
+'''
+DNAC_NAME="denlab-en-dnac.cisco.com"
+
+#
+# DNAC_IP: IP address of a DNAC cluster
+#
+DNAC_IP="10.8.10.20"
+
+#
+# DNAC_VERSION: used for setting the resource path of API calls based upon
+#               the version of the DNAC cluster.
+#
+DNAC_VERSION="1.2.8"
+
+#
+# DNAC_PORT: TCP port used to communicate with DNAC's API
+#
+DNAC_PORT="443"
+
+#
+# DNAC_USER: Name of a DNAC account with administrative privileges
+#
+DNAC_USER=""
+
+#
+# DNAC_PASSWD: Password for the DNAC_USER
+#
+DNAC_PASSWD=""
+
+#
+# DNAC_CONTENT_TYPE: Data format with DNAC should respond with
+#                    Valid values include: "application/json"
+#                                          "application/xml"
+#
+DNAC_CONTENT_TYPE="application/json"
+
