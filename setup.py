ec54dfc3259828c51fe53969c90d39f4ded84693
rsayle@cisco.com <rsayle@cisco.com>
2019-03-07 16:48:41 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: Cisco Sample Code License',
+        'License :: Other/Proprietary License',
         'Operating System :: OS Independent',
         'Topic :: System :: Networking',
         'Topic :: Utilities',
